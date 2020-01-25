40bf1da703c09bc68b0b94f51bdf2445741389bc
Davi Holanda <daviht7@gmail.com>
2020-01-24 23:22:39 -0300

    primeiro commit

---
+++ src/test/java/com/mc/algamoney/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.algamoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
