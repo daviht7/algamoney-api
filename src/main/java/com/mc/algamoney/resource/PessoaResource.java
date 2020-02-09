fe385632c5ff3639bd2eff9d1a12c687a5224aaa
Davi Holanda <daviht7@gmail.com>
2020-02-09 11:50:38 -0300

    finishing put person and create clas service

--- src/main/java/com/mc/algamoney/resource/PessoaResource.java
+++ src/main/java/com/mc/algamoney/resource/PessoaResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mc.algamoney.event.ResourceCreatedEvent;
 import com.mc.algamoney.model.Pessoa;
 import com.mc.algamoney.repository.PessoaRepository;
+import com.mc.algamoney.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -31,6 +33,9 @@ public class PessoaResource {
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@GetMapping
 	public List<Pessoa> listarTodos() {	
@@ -49,6 +54,13 @@ public class PessoaResource {
 	public void deletar(@PathVariable Long codigo) {	
 		pessoaRepository.deleteById(codigo);
 	} 
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		
+		return ResponseEntity.ok(pessoaService.atualizar(codigo, pessoa));
+		
+	}
 	
 
 }
---
+++ src/main/java/com/mc/algamoney/service/PessoaService.java
@@ -0,0 +1,35 @@
+package com.mc.algamoney.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.mc.algamoney.model.Pessoa;
+import com.mc.algamoney.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa atualizar(Long codigo,Pessoa pessoa) {
+		
+		Optional<Pessoa> p = pessoaRepository.findById(codigo);
+		
+		if(!p.isPresent()) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(pessoa,p.get(), "codigo");
+		
+		Pessoa pessoaSalva = pessoaRepository.save(p.get());
+		
+		return pessoaSalva;
+		
+	}
+	
+
+}
