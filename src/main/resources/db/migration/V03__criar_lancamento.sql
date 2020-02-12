28971e706bb3cdf38f09c3accef136accb6ef02f
Davi Holanda <daviht7@gmail.com>
2020-02-11 22:33:03 -0300

    generation table lancamento

---
+++ src/main/resources/db/migration/V03__criar_lancamento.sql
@@ -0,0 +1,13 @@
+CREATE TABLE lancamento(
+		codigo SERIAL PRIMARY KEY,
+		descricao VARCHAR(50) NOT NULL,
+		data_vencimento DATE NOT NULL,
+		data_pagamento DATE,
+		valor DECIMAL(10,2) NOT NULL,
+		observacao VARCHAR(100),
+		tipo VARCHAR(20) NOT NULL,
+	    codigo_categoria BIGINT ,
+		codigo_pessoa BIGINT,
+		FOREIGN KEY (codigo_categoria) REFERENCES categoria(codigo),
+		FOREIGN KEY (codigo_pessoa) REFERENCES pessoa(codigo)
+);
